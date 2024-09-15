83fa8a7b3f43c9b7c3ed3628a237c83350d2aa9f
BIJON SAHA <bijon176193@gmail.com>
2024-09-15 21:29:30 +0600

    Nodejs mysql route controller structure added

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
-import mysql from 'mysql';
+import mysql from 'mysql'
+
 export const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'Blog'
+    database: 'blog',
 });
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-import express from  'express';
+import express from  "express";
+import postRoutes from "./routes/posts.js"
 
 const app = express();
-app.use(express.json);
 
-app.listen(4000, () =>{
-    console.log("Connected to database");
+app.use(express.json());
+app.use('/api/posts', postRoutes);
+
+//App listen
+app.listen(8800,()=>{
+    console.log("Server is running on port 8800");
 })
