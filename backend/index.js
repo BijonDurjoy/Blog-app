9f8b51c8631630774edfb1874bb3215aa745927c
BIJON SAHA <bijon176193@gmail.com>
2024-09-18 20:17:58 +0600

    Test

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 import express from  "express";
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173' // Replace this with the frontend's origin
+}));
+
 app.use(express.json());
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 
 //App listen
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+    password: '',
+  })
+
+  const handleChange = e => {
+    setInputs(prev=>({...prev, [e.target.name]: [e.target.value]}))
+  }
+
+  const handleSubmit = async e =>{
+    e.preventDefault()
+    try{
+      const res = await axios.post("http://localhost:4500/api/auth/register",inputs)
+      console.log(res);
+    }catch(err){
+      console.log(err)
+    }
+  }
+  console.log(inputs)
   return (
     <div className='auth'>
       <h1>Welcome to the Register page</h1>
       <form>
-        <input required type="email" placeholder='email' />
-        <input required type="text" placeholder='username'/>
-        <input required type="password" placeholder='password'/>
-        <button>Register</button>
+        <input required type="text" placeholder='username' name='username' onChange={handleChange}/>
+        <input required type="email" placeholder='email' name='email' onChange={handleChange}/>
+        <input required type="password" placeholder='password' name='password' onChange={handleChange} />
+        <button onClick={handleSubmit}>Register</button>
         <span>
-          Dont have an account? <a href="http://localhost:5173/login">Login</a>
+          Do you have an account? <Link to="/login">Login</Link>
         </span>
       </form>
     </div>
