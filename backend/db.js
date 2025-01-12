1f5f92896f55719e32b2b5532f9ed9d21570a423
BijonDurjoy <bijon176193@gmail.com>
2025-01-12 12:03:42 +0600

    Databes connected to the railway

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,23 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-export {db};
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.message);
+  } else {
+    console.log('Connected to the database!');
+  }
+});
+
+export { db };
+
